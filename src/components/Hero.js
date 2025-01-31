48d324b1165f476babd93e545e72c395e284c8c8
awheel11 <awheel11@wgu.edu>
2025-01-31 00:27:19 -0700

    Restructured images so they will appear on hosted site.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,38 @@
 import React from "react";
+import profilePic from "../static/profile-pic.jpg";
+import locationIcon from "../static/location-icon.png";
+import educationIcon from "../static/education.png";
+import githubIcon from "../static/github-icon.png";
+import linkedinIcon from "../static/linkedin-icon.png";
+import emailIcon from "../static/email-icon.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="container">
-        <img src="${process.env.PUBLIC_URL}/static/profile-pic.jpg" alt="Profile Picture" className="profile-pic" />
+        <img src={profilePic} alt="Profile Picture" className="profile-pic" />
         <div className="hero-content">
           <h1>Andre Wheeler</h1>
           <h2>Software Engineer</h2>
           <div className="location">
-            <img src="${process.env.PUBLIC_URL}/static/location-icon.png" alt="Location" className="location-icon" />
+            <img src={locationIcon} alt="Location" className="location-icon" />
             <p>Phoenix, AZ</p>
           </div>
           <div className="location">
-            <img src="${process.env.PUBLIC_URL}/static/education.png" alt="Education" className="location-icon" />
+            <img src={educationIcon} alt="Education" className="location-icon" />
             <p>B.S. Software Engineering</p>
           </div>
           <div className="links">
             <a href="https://github.com/Drevelops" target="_blank" rel="noopener noreferrer">
-              <img src="${process.env.PUBLIC_URL}/static/github-icon.png" alt="GitHub" />
+              <img src={githubIcon} alt="GitHub" />
               <p>GitHub</p>
             </a>
             <a href="https://linkedin.com/in/andre-wheeler" target="_blank" rel="noopener noreferrer">
-              <img src="${process.env.PUBLIC_URL}/static/linkedin-icon.png" alt="LinkedIn" />
+              <img src={linkedinIcon} alt="LinkedIn" />
               <p>LinkedIn</p>
             </a>
-            <a href="/static/resume.pdf" download="resume.pdf">
-              <img src="${process.env.PUBLIC_URL}/static/resume-icon.png" alt="Resume" />
-              <p>Resume</p>
-            </a>
             <a href="mailto:andre.d.wheeler@gmail.com">
-              <img src="${process.env.PUBLIC_URL}/static/email-icon.png" alt="Email" />
+              <img src={emailIcon} alt="Email" />
               <p>Email</p>
             </a>
           </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import githubIcon from "../static/github-icon.png";
 
 const Projects = () => {
   return (
@@ -9,7 +10,7 @@ const Projects = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
         <div className="project-links">
           <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
-          <img  src="${process.env.PUBLIC_URL}/static/github-icon.png" alt="GitHub Code" /> 
+          <img  src={githubIcon} alt="GitHub Code" /> 
           </a> 
         </div>
       </div>
@@ -18,7 +19,7 @@ const Projects = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
         <div className="project-links">
           <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
-          <img  src="${process.env.PUBLIC_URL}/static/github-icon.png" alt="GitHub Code" /> 
+          <img  src={githubIcon} alt="GitHub Code" /> 
           </a> 
         </div>
       </div>
@@ -27,7 +28,7 @@ const Projects = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
         <div className="project-links">
           <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
-          <img  src="${process.env.PUBLIC_URL}/static/github-icon.png" alt="GitHub Code" /> 
+          <img  src={githubIcon} alt="GitHub Code" /> 
           </a> 
         </div>
       </div>

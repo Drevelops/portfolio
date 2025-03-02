b64c184c93d4f87c1b7872c6ed73894b8ae4abec
Drevelops <andre_wheeler@icloud.com>
2025-03-02 13:44:54 -0700

    Add live demo GIF to TaskManagerApp project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import githubIcon from "../static/github-icon.png";
+import LiveDemo from "../static/LiveDemoTaskManager.gif";
 
 const Projects = () => {
   return (
@@ -8,12 +9,10 @@ const Projects = () => {
       <div>
         <h3>Project 1</h3>
         <p>A modern, feature-rich task management application that enables users to efficiently organize and track their tasks through different stages of completion. Built with React and Spring Boot, this application offers a seamless drag-and-drop interface and robust backend functionality.</p>
+        <img src={LiveDemo} alt="Live Demo" />
         <div className="project-links">
           <a href="https://github.com/Drevelops/TaskManagerApp" target="_blank" rel="noopener noreferrer">
           <img  src={githubIcon} alt="GitHub Code" /> 
-          </a>
-          <a href="http://taskmngr-frontend-andre.s3-website-us-west-1.amazonaws.com/kanban" target="_blank" rel="noopener noreferrer">
-          
           </a>
         </div>
       </div>

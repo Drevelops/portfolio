fc50b6612470aeecce746938043be898e5c891dc
Drevelops <andre_wheeler@icloud.com>
2025-03-02 12:20:20 -0700

    Update Projects section with TaskManagerApp details and live site link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,29 +7,14 @@ const Projects = () => {
       <h2>Projects</h2>
       <div>
         <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
+        <p>A modern, feature-rich task management application that enables users to efficiently organize and track their tasks through different stages of completion. Built with React and Spring Boot, this application offers a seamless drag-and-drop interface and robust backend functionality.</p>
         <div className="project-links">
-          <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Drevelops/TaskManagerApp" target="_blank" rel="noopener noreferrer">
           <img  src={githubIcon} alt="GitHub Code" /> 
-          </a> 
-        </div>
-      </div>
-      <div>
-        <h3>Project 2</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
-        <div className="project-links">
-          <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
-          <img  src={githubIcon} alt="GitHub Code" /> 
-          </a> 
-        </div>
-      </div>
-      <div>
-        <h3>Project 3</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis mi vitae orci maximus, ut efficitur erat mattis. Integer vehicula luctus lacinia. Mauris sollicitudin, urna quis porttitor ornare, felis arcu commodo orci, in blandit ex lectus ut enim. Sed a turpis tempus, varius lorem id, aliquet enim. Curabitur in tortor nec ipsum bibendum lacinia. Sed eget diam in elit lobortis pellentesque. Ut et suscipit sapien, ut rutrum erat. Sed bibendum magna dui, non luctus tellus iaculis vitae.</p>
-        <div className="project-links">
-          <a href="https://github.com/your-username/nba-tracker" target="_blank" rel="noopener noreferrer">
-          <img  src={githubIcon} alt="GitHub Code" /> 
-          </a> 
+          </a>
+          <a href="http://taskmngr-frontend-andre.s3-website-us-west-1.amazonaws.com/kanban" target="_blank" rel="noopener noreferrer">
+          
+          </a>
         </div>
       </div>
     </section>
